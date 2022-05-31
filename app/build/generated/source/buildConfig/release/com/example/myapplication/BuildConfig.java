dea52c894322304a5f7f46194bfdf1f4678ff623
akmal09 <akmal.119140203@student.itera.ac.id>
2022-05-31 21:01:38 +0600

    input build config karena ada token api

---
+++ app/build/generated/source/buildConfig/release/com/example/myapplication/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.myapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.myapplication";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String Authorization = "token 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";
+}
